5bcaf4299d34907971b5c362168cc3deeec99157
David Molina <davidmolina2810@gmail.com>
2020-08-09 00:34:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-#1. Get array args
+#1. array args
 #2. array1.concat(array2)
 def using_concat(array1, array2)
     array1.concat(array2)
@@ -10,8 +10,14 @@ def using_insert(array, element)
     array.insert(4, element)
   end
 
-#1. Get array
+#1. array arg 
 #2. array.uniq to get rid of duplicates
 def using_uniq(arr)
   arr.uniq
+end
+
+#1. array arg 
+#2. array.flatten to return array of strings
+def using_flatten(arr)
+  arr.flatten
 end
