b5ed7426aab9454c94586913f48b22c94d8df636
David Molina <davidmolina2810@gmail.com>
2020-08-09 00:23:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,13 @@
 #1. Get strings from array args
 #2. Set counter
 #3. While counter < array2.length
-  #4. array1.push(array2[counter])
+  #4. array1.concat(array2[counter])
   #5. counter += 1
 #6 return array1
 def using_concat(array1, array2)
   index = 0
   while index < array2.length do
-    array1.push(array2[index])
+    array1.concat(array2[index])
     index += 1
   end
 end
